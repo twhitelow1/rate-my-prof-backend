5c1e6c540816c10c77a6c960bba6edadd6a9ed0d
Todd Whitelow <todd@whitelow.tech>
2021-01-12 00:50:16 -0700

    fix request params and body references

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -4,10 +4,10 @@ module.exports = {
   create(req, res) {
     return Review
       .create({
-        professorid: req.body.professorid,
-        rating: req.params.rating,
-        text: req.params.text,
-        reviewer: req.params.reviewer,
+        professorid: req.params.professorId,
+        rating: req.body.rating,
+        text: req.body.text,
+        reviewer: req.body.reviewer,
       })
       .then(review => res.status(201).send(review))
       .catch(error => res.status(400).send(error));
