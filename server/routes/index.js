02ca66fb962e2cbb243b61b2fc8780735c4fef6c
Todd Whitelow <todd@whitelow.tech>
2021-01-11 23:50:00 -0700

    add routes folder, index, and professor create route

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const professorsController = require('../controllers').professors;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Rate My Professor API!',
+  }));
+
+  app.post('/api/professors', professorsController.create);
+};
