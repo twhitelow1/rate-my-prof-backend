6d61b6776bf2b3e7ed57e24599a3fa0fcfba783f
Todd Whitelow <todd@whitelow.tech>
2021-01-17 20:53:30 -0700

    change foreign key in review model

--- server/models/review.js
+++ server/models/review.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
 
   Review.associate = (models) => {
     Review.hasMany(models.Professor, {
-      foreignKey: 'id',
+      foreignKey: 'professor_id',
     });
   };
 
