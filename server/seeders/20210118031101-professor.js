6fef1c42b46b9e1ed579f2862cfb8dee2e553903
Todd Whitelow <todd@whitelow.tech>
2021-01-17 20:14:13 -0700

    add prof seeds file

---
+++ server/seeders/20210118031101-professor.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Professors', [{
+      name: 'John Mayer',
+      title: 'Professor',
+      school: 'School of Rock',
+      department: 'Vocals',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {
+      name: 'Jerry Garcia',
+      title: 'Lead Professor',
+      school: 'School of Rock',
+      department: 'Guitar',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'Jerry Garcia',
+      title: 'Lead Professor',
+      school: 'School of Rock',
+      department: 'Guitar',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    );
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Professors', null, {});
+  }
+};
