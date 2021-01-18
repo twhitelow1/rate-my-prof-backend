73e3b465b9171cd78b45b474f2973bf4d1622dbf
Todd Whitelow <todd@whitelow.tech>
2021-01-17 19:54:23 -0700

    update connections

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Require our routes into the application
-require('./server/routes')(app);
+//development only
+if ('development' === app.)
+
+  //Require our routes into the application
+  require('./server/routes')(app);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
